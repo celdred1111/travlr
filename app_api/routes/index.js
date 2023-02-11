f6f0c499991215a18cace9986389e529d44ee238
celdred1111 <celred1111@gmail.com>
2023-02-11 16:15:39 -0500

    Module6

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,10 +5,12 @@ const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripslist);
+    .get(tripsController.tripslist)
+    .post(tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindCode);
+    .get(tripsController.tripsFindCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
