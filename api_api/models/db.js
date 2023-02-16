9b0b6570587ba82d3ab9996d2a9e2add78e404ef
celdred1111 <celred1111@gmail.com>
2023-02-16 12:01:57 -0500

    module7

--- api_api/models/db.js
+++ api_api/models/db.js
@@ -1,19 +1,38 @@
 const mongoose = require('mongoose');
-let dbURI = 'mongodb://localhost/travlr';
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURL = `mongodb://${host}/travlr`;
+const readLine = require('readline');
+
 if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGODB_URI;
+  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
 }
-mongoose.connect(dbURI);
+
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }), 1000);
+}
 
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+  console.log('connected');
 });
+
 mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error:', err);
+  console.log('error: ' + err);
+  return connect();
 });
+
 mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
+  console.log('disconnected');
 });
+
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
 
 const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close( () => {
@@ -22,23 +41,24 @@ const gracefulShutdown = (msg, callback) => {
   });
 };
 
-// For nodemon restarts                                 
 process.once('SIGUSR2', () => {
   gracefulShutdown('nodemon restart', () => {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
-// For app termination
 process.on('SIGINT', () => {
   gracefulShutdown('app termination', () => {
     process.exit(0);
   });
 });
-// For Heroku app termination
 process.on('SIGTERM', () => {
   gracefulShutdown('Heroku app shutdown', () => {
     process.exit(0);
   });
 });
 
-require('./models/travlr');
+connect();
+
+// bring in the Mongoose schema
+require('./travlr');
+require('./user');
